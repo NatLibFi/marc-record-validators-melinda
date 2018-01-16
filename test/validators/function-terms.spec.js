a90a46d818866b2201de9ee4c94625f8b938595f
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2018-01-16 10:53:22 +0200

    Add test to reproduce the 'undefined' bug in function-terms.js

--- test/validators/function-terms.spec.js
+++ test/validators/function-terms.spec.js
@@ -1,6 +1,6 @@
 /**
  *
- * @licstart  The following is the entire license notice for the JavaScript code in this file. 
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
  *
  * Melinda-related validators for @natlibfi/marc-record-validate
  *
@@ -12,12 +12,12 @@
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -59,25 +59,25 @@ function factory(chai, chaiAsPromised, MarcRecord, validator_factory)
   chai.use(chaiAsPromised);
 
   describe('function-terms', function() {
-    
+
     it('Should be the expected object', function() {
       expect(validator_factory).to.be.an('object');
       expect(validator_factory.name).to.be.a('string');
       expect(validator_factory.factory).to.be.a('function');
     });
-    
+
     describe('#factory', function() {
-      
-      it('Should return the expected object', function() {        
+
+      it('Should return the expected object', function() {
         expect(validator_factory.factory()).to.be.an('object').and.to
           .respondTo('validate').and.to
-          .respondTo('fix');        
+          .respondTo('fix');
       });
-      
+
       describe('object', function() {
-        
+
         describe('#validate', function() {
-          
+
           it('Should resolve with an array', function() {
             return expect(validator_factory.factory().validate(new MarcRecord())).to.eventually.eql([]);
           });
@@ -117,19 +117,19 @@ function factory(chai, chaiAsPromised, MarcRecord, validator_factory)
               message: 'Invalid function term in 100$e',
               field: record.fields[0]
             }]);
-            
+
           });
 
         });
-        
+
         describe('#fix', function() {
-          
+
           it('Should resolve with an array', function() {
             return expect(validator_factory.factory().fix(new MarcRecord())).to.eventually.be.an('array');
           });
-          
+
           it('Should fix the record', function() {
-            
+
             var record = new MarcRecord({
               fields: [{
                 tag: '100',
@@ -169,17 +169,97 @@ function factory(chai, chaiAsPromised, MarcRecord, validator_factory)
               }]);
               expect(record_original).to.not.eql(record.toJsonObject());
               expect(record.fields).to.eql([field_modified]);
-            
             });
-            
           });
-          
-        });
-        
-      });
-      
-    });
-    
-  });
 
+          it('Should be able to handle "esitt."-type terms', function() {
+
+            var record = new MarcRecord({
+              fields: [{
+                tag: '700',
+                subfields: [
+                  {
+                    code: 'a',
+                    value: 'Laarko, Petri,'
+                  },
+                  {
+                    code: 'e',
+                    value: 'säveltäjä,'
+                  },
+                  {
+                    code: 'e',
+                    value: 'sanoittaja.'
+                  }
+                ]
+              },
+                {
+                  tag: '700',
+                  subfields: [
+                    {
+                      code: 'a',
+                      value: 'Helminen, Taneli,'
+                    },
+                    {
+                      code: 'e',
+                      value: 'esittäjä'
+                    },
+                    {
+                      code: 'e',
+                      value: 'esitt.'
+                    }
+                  ]
+
+                }]
+            });
+
+            var record_modified = new MarcRecord({
+              fields: [{
+                tag: '700',
+                subfields: [
+                  {
+                    code: 'a',
+                    value: 'Laarko, Petri,'
+                  },
+                  {
+                    code: 'e',
+                    value: 'säveltäjä,'
+                  },
+                  {
+                    code: 'e',
+                    value: 'sanoittaja.'
+                  }
+                ]
+              },
+                {
+                  tag: '700',
+                  subfields: [
+                    {
+                      code: 'a',
+                      value: 'Helminen, Taneli,'
+                    },
+                    {
+                      code: 'e',
+                      value: 'esittäjä.'
+                    }
+                  ]
+                }]
+            }),
+            record_original_json = record.toJsonObject(),
+            record_modified_json = record_modified.toJsonObject();
+
+            return validator_factory.factory().fix(record).then(function(results) {
+
+              // expect(results).to.eql([{
+              //   'type': 'modifyField',
+              //   'old': record_original_json,
+              //   'new': record_modified_json[0]
+              // }]);
+              expect(record_original_json).to.not.eql(record.toJsonObject());
+              expect(record.toJsonObject()).to.eql(record_modified_json);
+            });
+          });
+        });
+      });
+    });
+  });
 }
