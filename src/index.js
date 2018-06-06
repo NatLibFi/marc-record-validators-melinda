652a7732a4a81274402f53d0afeb331b007a91b1
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2018-06-06 11:53:20 +0300

    Add new implementation

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
 /**
  *
- * @licstart  The following is the entire license notice for the JavaScript code in this file. 
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
  *
- * Melinda-related validators for @natlibfi/marc-record-validate
+ * MARC record validators used in Melinda
  *
- * Copyright (c) 2014-2017 University Of Helsinki (The National Library Of Finland)
+ * Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
  *
  * This file is part of marc-record-validators-melinda
  *
- * marc-record-validators-melinda is free software: you can redistribute it and/or modify
+ * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful,
+ *
+ * marc-record-validators-melinda is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *  
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @licend  The above is the entire license notice
  * for the JavaScript code in this file.
  *
- **/
+ */
 
-(function() {
+/* eslint-disable no-unused-vars */
 
-  'use strict';
+'use strict';
 
-  require('../legal-term-factory')(require('../../../lib/validators/nodejs/legal-term'), require('http-client-x-mock/lib/nodejs/main'));
+import DoubleCommas from './double-commas';
+import DuplicatesInd1 from './duplicates-ind1';
 
-}());
+export {DoubleCommas, DuplicatesInd1};
