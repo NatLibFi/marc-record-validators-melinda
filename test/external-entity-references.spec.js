6bba54842a2c66d69b1dbf37b7d3f57cfbeb8898
harri-h <harri.huhtala@mystes.fi>
2018-06-14 13:22:11 +0300

    initial test setup

---
+++ test/external-entity-references.spec.js
@@ -0,0 +1,134 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
+ *
+ * MARC record validators used in Melinda
+ *
+ * Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
+ *
+ * This file is part of marc-record-validators-melinda
+ *
+ * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * marc-record-validators-melinda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ *
+ */
+
+/* eslint-disable no-undef, max-nested-callbacks, no-unused-expressions */
+
+'use strict';
+import {expect} from 'chai';
+import MarcRecord from 'marc-record-js';
+import validatorFactory from '../src/empty-fields';
+
+const endpoint = 'https://foo.bar';
+const prefixPattern = '/\(FOOBAR\)/'
+const fields = {
+  773: ['w'],
+  833: ['w', 'p']
+}
+
+describe('empty-fields', () => {
+	it('Creates a validator', async () => {
+		const validator = await validatorFactory();
+
+		expect(validator)
+			.to.be.an('object')
+			.that.has.any.keys('description', 'validate');
+
+		expect(validator.description).to.be.a('string');
+		expect(validator.validate).to.be.a('function');
+	});
+
+	describe('#validate', () => {
+		it('Finds the record valid', async () => {
+			const validator = await validatorFactory();
+			const record = new MarcRecord({
+          fields: [
+            {
+              tag: '001',
+              value: '123456'
+            },
+            {
+              tag: '035',
+              subfields: [
+                {
+                  code: 'a',
+                  value: '(FI-MELINDA)123456'
+                }
+              ]
+            },
+            {
+              tag: '773',
+              subfields: [
+                {
+                  code: 'w',
+                  value: '(FOOBAR)4567'
+                }
+              ]
+            },
+            {
+              tag: '833',
+              subfields: [
+                {
+                  code: 'p',
+                  value: '(FOOBAR)4567'
+                }
+              ]
+            }
+          ]
+        });
+			const result = await validator.validate(record);
+
+			expect(result).to.eql({valid: true});
+    });
+  });
+
+  describe('#validate', () => {
+		it('Finds the record invalid', async () => {
+			const validator = await validatorFactory();
+			const record = new MarcRecord({
+				fields: [
+          {
+            tag: '001',
+            value: '123456'
+          },
+          {
+            tag: '035',
+            subfields: [
+              {
+                code: 'a',
+                value: '(FI-MELINDA)123456'
+              }
+            ]
+          },
+          {
+            tag: '773',
+            subfields: [
+              {
+                code: 'w',
+                value: '(FI-MELINDA)4567'
+              }
+            ]
+          }
+        ]
+			});
+			const result = await validator.validate(record);
+
+			expect(result).to.eql({valid: true});
+    });
+  });
+
+});
