5d4bbdce6eb8f60d4dff0a883f41fc8a81a01a5b
Harri-H <harri.huhtala@mystes.fi>
2018-08-13 13:13:38 +0300

    External entity references (#28)
    Implement Melinda external entity reference validator.

---
+++ src/resolvable-ext-references-melinda.js
@@ -0,0 +1,124 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
+ *
+ * MARC record validators used in Melinda
+ *
+ * Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
+ *
+ * This file is part of marc-record-validators-melinda
+ *
+ * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * marc-record-validators-melinda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ *
+ */
+
+import {parseString} from 'xml2js';
+import fetch from 'node-fetch';
+import {last} from 'lodash';
+
+export default async function ({endpoint, prefixPattern, fields}) {
+	if (typeof endpoint === 'string' && prefixPattern instanceof RegExp && typeof fields === 'object') {
+		return {
+			description: 'Checks if Melinda entity references are resolvable',
+			validate
+		};
+	}
+	throw new Error('Error in validation parameters');
+
+	async function validate(record) {
+		const validateResult = await validateRecord(record);
+		return validateResult;
+	}
+
+	function validateRecord(record) {
+		const removedPrefixes = [];
+
+		// Filter matching field keys from record.fields
+		const subfields = record.fields.reduce((prev, current) => {
+			Object.keys(fields).forEach(key => {
+				if (key === current.tag) {
+					prev.push(current);
+				}
+			});
+			return prev;
+		}, []);
+
+		// Filter matching objects from subfields
+		const matchingTags = [...subfields].reduce((prev, current) => {
+			Object.keys(fields).forEach(key => {
+				if (key === current.tag) {
+					current.subfields.filter(item => {
+						if (Object.values(fields[key]).filter(value => value === item.code)[0]) {
+							prev.push({tag: current.tag, code: item.code, value: item.value});
+						}
+						return prev;
+					});
+				}
+			});
+			return prev;
+		}, []);
+
+		// Matching prefixPattern is removed from object value field.
+		matchingTags.forEach(obj => {
+			if (prefixPattern.test(obj.value)) {
+				obj.value = obj.value.replace(prefixPattern, '');
+				removedPrefixes.push(obj);
+			}
+		});
+		return resolveValidation(removedPrefixes);
+	}
+
+	function resolveValidation(removedPrefixes) {
+		// If matching prefixPatterns found make an API call
+		if (removedPrefixes.length > 0) {
+			return validateMatchingTags(removedPrefixes).then(result => {
+				return result;
+			});
+		}
+		return {valid: true, messages: []};
+	}
+
+	async function validateMatchingTags(tags) {
+		const resolved = await Promise.all(tags.map(obj => {
+			return getData(obj.value).then(valid => {
+				return Object.assign({valid}, obj);
+			});
+		}));
+
+		if (resolved.every(value => value.valid === true)) {
+			return {valid: true, messages: []};
+		}
+
+		return {valid: false, messages: resolved.map(obj => `Field ${obj.tag}$${obj.code} with value ${obj.value} is not resolvable`)};
+	}
+
+	async function getData(recID) {
+		const queryParam = '?operation=searchRetrieve&maximumRecords=2&version=1&query=rec.id=';
+
+		const response = await fetch(`${endpoint}${queryParam}${recID}`);
+
+		const xml = await response.text();
+
+		return new Promise(resolve => {
+			parseString(xml, (err, result) => {
+				const record = last(result['zs:searchRetrieveResponse']['zs:records']);
+				const position = parseInt(last(record['zs:record'])['zs:recordPosition'][0], 10);
+				resolve(position === 1 && !err);
+			});
+		});
+	}
+}
---
+++ test/resolvable-ext-references-melinda.spec.js
@@ -0,0 +1,214 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
+ *
+ * MARC record validators used in Melinda
+ *
+ * Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
+ *
+ * This file is part of marc-record-validators-melinda
+ *
+ * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * marc-record-validators-melinda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ *
+ */
+
+/* eslint-disable no-undef, max-nested-callbacks, no-unused-expressions */
+
+'use strict';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import MarcRecord from 'marc-record-js';
+import fetchMock from 'fetch-mock';
+import * as testContext from '../src/resolvable-ext-references-melinda'; /* eslint-disable-line import/named */
+import {fixture5000, fixture9550, fixture1000} from './fixtures/resolvable-ext-references-melinda';
+
+const {expect} = chai;
+chai.use(chaiAsPromised);
+
+const endpoint = 'http://melinda.kansalliskirjasto.fi:210/fin01';
+const queryParam = '?operation=searchRetrieve&maximumRecords=2&version=1&query=rec.id=';
+const prefixPattern = /^\(FOOBAR\)/;
+const fields = {
+	773: ['w'],
+	833: ['w', 'p']
+};
+
+describe('resolvable-ext-references-melinda', () => {
+	afterEach(() => {
+		testContext.default.__ResetDependency__('fetch');
+	});
+
+	it('Creates a validator', async () => {
+		const validator = await testContext.default({endpoint, prefixPattern, fields});
+
+		expect(validator)
+			.to.be.an('object')
+			.that.has.any.keys('description', 'validate');
+
+		expect(validator.description).to.be.a('string');
+		expect(validator.validate).to.be.a('function');
+	});
+
+	it('Throws an error when prefixPattern not provided', async () => {
+		const validator = await testContext.default({endpoint, prefixPattern, fields});
+		await expect(validator.validate()).to.be.rejectedWith(Error, 'Cannot read property \'fields\' of undefined');
+	});
+
+	describe('#validate', () => {
+		it('Finds prefixPattern on record and removes it', async () => {
+			const mock = fetchMock.sandbox();
+			mock.get(`${endpoint}${queryParam}5000`, fixture5000);
+			mock.get(`${endpoint}${queryParam}9550`, fixture9550);
+
+			testContext.default.__Rewire__('fetch', mock);
+			const validator = await testContext.default({endpoint, prefixPattern, fields});
+
+			const record = new MarcRecord({
+				fields: [
+					{
+						tag: '001',
+						value: '123456'
+					},
+					{
+						tag: '035',
+						subfields: [
+							{
+								code: 'a',
+								value: '(FI-MELINDA)123456'
+							}
+						]
+					},
+					{
+						tag: '773',
+						subfields: [
+							{
+								code: 'w',
+								value: '(FOOBAR)5000'
+							}
+						]
+					},
+					{
+						tag: '833',
+						subfields: [
+							{
+								code: 'p',
+								value: '(FOOBAR)9550'
+							},
+							{
+								code: 'c',
+								value: '(FI-MELINDA)8850'
+							}
+						]
+					}
+				]
+			});
+			const result = await validator.validate(record);
+
+			expect(result).to.eql({valid: true, messages: []});
+		});
+
+		it('Finds no matching prefixPattern on record', async () => {
+			const mock = fetchMock.sandbox();
+
+			mock.get(`${endpoint}5000`, fixture5000);
+
+			testContext.default.__Rewire__('fetch', mock);
+			const validator = await testContext.default({endpoint, prefixPattern, fields});
+
+			const record = new MarcRecord({
+				fields: [
+					{
+						tag: '001',
+						value: '123456'
+					},
+					{
+						tag: '035',
+						subfields: [
+							{
+								code: 'a',
+								value: '(FI-MELINDA)123456'
+							}
+						]
+					},
+					{
+						tag: '773',
+						subfields: [
+							{
+								code: 'w',
+								value: '(FI-MELINDA)123456'
+							}
+						]
+					},
+					{
+						tag: '833',
+						subfields: [
+							{
+								code: 'p',
+								value: '(FI-MELINDA)2620'
+							},
+							{
+								code: 'w',
+								value: '(FI-MELINDA)8850'
+							}
+						]
+					}
+				]
+			});
+			const result = await validator.validate(record);
+
+			expect(result).to.eql({valid: true, messages: []});
+		});
+
+		it('Finds prefixPattern on record but values not resolvable', async () => {
+			const mock = fetchMock.sandbox();
+			mock.get(`${endpoint}${queryParam}1000`, fixture1000);
+
+			testContext.default.__Rewire__('fetch', mock);
+			const validator = await testContext.default({endpoint, prefixPattern, fields});
+
+			const record = new MarcRecord({
+				fields: [
+					{
+						tag: '001',
+						value: '123456'
+					},
+					{
+						tag: '035',
+						subfields: [
+							{
+								code: 'a',
+								value: '(FI-MELINDA)123456'
+							}
+						]
+					},
+					{
+						tag: '773',
+						subfields: [
+							{
+								code: 'w',
+								value: '(FOOBAR)1000'
+							}
+						]
+					}
+				]
+			});
+			const result = await validator.validate(record);
+
+			expect(result).to.eql({valid: false, messages: ['Field 773$w with value 1000 is not resolvable']});
+		});
+	});
+});
