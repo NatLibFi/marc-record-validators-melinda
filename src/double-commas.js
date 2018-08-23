88dd2cb1a951af2a41339e66a9d523428b18c098
Olli Pajuluoma <olli.pajuluoma@leadin.fi>
2018-08-23 13:49:22 +0300

    Reverted e7660c9260bfcd038e9135530fc39845379e2fc2 for merging to main

---
+++ src/double-commas.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
+ *
+ * MARC record validators used in Melinda
+ *
+ * Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
+ *
+ * This file is part of marc-record-validators-melinda
+ *
+ * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * marc-record-validators-melinda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ *
+ */
+
+'use strict';
+
+export default async function () {
+	return {
+		description: 'Handle double commas in 700$e-subfields',
+		validate: async record => ({
+			valid: !record
+				.get(/^700$/)
+				.some(f =>
+					f.subfields.every(sf => sf.code === 'e' && /,,/.test(sf.value))
+				)
+		}),
+		fix: async record =>
+			record.get(/^700$/).forEach(f =>
+				f.subfields.filter(sf => sf.code === 'e').forEach(sf => {
+					sf.value = sf.value.replace(/,,/, ',');
+				})
+			)
+	};
+}
---
+++ src/duplicates-ind1.js
@@ -0,0 +1,65 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
+ *
+ * MARC record validators used in Melinda
+ *
+ * Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
+ *
+ * This file is part of marc-record-validators-melinda
+ *
+ * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * marc-record-validators-melinda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ *
+ */
+
+'use strict';
+
+export default async function (tagPattern) {
+	if (tagPattern instanceof RegExp) {
+		return {
+			description:
+				'Handles data fields that only differ in the first indicator',
+			validate: async record => ({valid: !record.fields.some(matches)}),
+			fix: async record =>
+				record.fields
+					.filter(matches)
+					.forEach(field => record.removeField(field))
+		};
+	}
+	throw new Error('No tagPattern provided');
+
+	function matches(field, index, fields) {
+		return (
+			tagPattern.test(field.tag) && field.ind1 === ' ' && hasDuplicate(field)
+		);
+
+		function hasDuplicate(a) {
+			return fields.some(
+				b =>
+					a !== b &&
+					a.tag === b.tag &&
+					a.ind1 !== b.ind1 &&
+					a.subfields.length === b.subfields.length &&
+					a.subfields.every(aSf =>
+						b.subfields.some(
+							bSf => aSf.code === bSf.code && aSf.value === bSf.value
+						)
+					)
+			);
+		}
+	}
+}
