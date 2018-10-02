b78965d572ea0f53aaeaa738e28c9322cf70668f
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2018-10-02 09:59:58 +0300

    Update empty fields validator Update empty fields validator to work with new @natlibfi/marc-record version

--- src/empty-fields.js
+++ src/empty-fields.js
@@ -1,83 +1,102 @@
 /**
- *
- * @licstart  The following is the entire license notice for the JavaScript code in this file.
- *
- * MARC record validators used in Melinda
- *
- * Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
- *
- * This file is part of marc-record-validators-melinda
- *
- * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * marc-record-validators-melinda is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @licend  The above is the entire license notice
- * for the JavaScript code in this file.
- *
- */
-
-/**
-* DEPRECATED: This validator is not needed because @natlibfi/marc-record doesn't allow fields with empty values
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* MARC record validators used in Melinda
+*
+* Copyright (C) 2014-2018 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of marc-record-validators-melinda
+*
+* marc-record-validators-melinda program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* marc-record-validators-melinda is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
 */
 
 /* eslint-disable require-await */
-import {find, isEmpty, omit} from 'lodash';
+const ERRORS = {
+	EMPTY_SUBFIELDS: 2,
+	MISSING_SUBFIELD_VALUES: 3
+};
 
 export default async function () {
 	return {
-		description: 'Handles empty fields **DEPRECATED**',
+		description: 'Handles empty fields',
 		validate,
 		fix
 	};
 
 	async function validate(record) {
-		const result = searchFields(record);
-		const isValid = result.find(obj => obj.valid === false);
-		return isValid === undefined ? {valid: true, messages: []} : omit(isValid, ['field']);
+		const errors = findErrors(record.fields);
+		if (errors.length > 0) {
+			return {
+				valid: false,
+				messages: errors.map(error => { // eslint-disable-line array-callback-return
+					switch (error.code) {
+						case ERRORS.EMPTY_SUBFIELDS:
+							return `Field ${error.field.tag} has no subfields`;
+						case ERRORS.MISSING_SUBFIELD_VALUES:
+							return `Field ${error.field.tag} has missing subfield values: ${error.emptySubfields.map(sf => sf.code).join()}`;
+						default:
+					}
+				})
+			};
+		}
+
+		return {valid: true};
 	}
 
 	async function fix(record) {
-		const fixFields = searchFields(record);
-		const isValid = fixFields.find(obj => obj.valid === false);
-
-		if ('subfields' in isValid.field && !isEmpty(isValid.field.subfields)) {
-			record.removeSubfield(find(isValid.field.subfields, {value: ''}), isValid.field);
-		} else {
-			record.removeField(isValid);
-		}
-	}
-	function searchFields(record) {
-		let validationResult;
-		record.fields.forEach(obj => {
-			validationResult = [emptyControlFields(obj), emptySubfieldValues(obj), emptySubfields(obj)];
+		const errors = findErrors(record.fields);
+		errors.forEach(error => {
+			if (error.code === ERRORS.MISSING_SUBFIELD_VALUES) {
+				if (error.emptySubfields.length === error.field.subfields.length) {
+					record.removeField(error.field);
+				} else {
+					error.emptySubfields.forEach(sf => {
+						record.removeSubfield(sf, error.field);
+					});
+				}
+			} else {
+				record.removeField(error.field);
+			}
 		});
-		return validationResult;
 	}
 
-	function emptyControlFields(field) {
-		const result = 'value' in field && field.value.length === 0;
-		return result === false ? {valid: true} : {valid: false, messages: [`Field ${field.tag} has empty value`], field};
-	}
+	function findErrors(fields) {
+		return fields.reduce((errors, field) => {
+			if (field.subfields) {
+				if (field.subfields.length === 0) {
+					return errors.concat({field, code: ERRORS.EMPTY_SUBFIELDS});
+				}
 
-	function emptySubfields(field) {
-		const result = field.subfields && field.subfields.length === 0;
-		return result === false ? {valid: true} : {valid: false, messages: [`Field ${field.tag} has no subfields`], field};
-	}
+				const subfieldsWithoutValue = field.subfields.filter(sf => {
+					return !sf.value;
+				});
 
-	function emptySubfieldValues(field) {
-		if (field.subfields) {
-			const result = field.subfields.filter(subfield => subfield.value.length === 0);
-			return result.length === 0 ? {valid: true} : {valid: false, messages: result.map(item => `Field ${field.tag}$${item.code} has empty value`), field};
-		}
+				if (subfieldsWithoutValue.length > 0) {
+					return errors.concat({
+						field,
+						emptySubfields: subfieldsWithoutValue,
+						code: ERRORS.MISSING_SUBFIELD_VALUES
+					});
+				}
+			}
+
+			return errors;
+		}, []);
 	}
 }
