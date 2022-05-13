b586339679a8239657a8f597513020eff430edef
nvolk <nicholas.volk@helsinki.fi>
2022-05-13 11:17:09 +0300

    Support 264a (#114)
    * Add rule for 264$a. Fix rule for 264$b.
    * Support punctuation type SPACECOLON, which is seen between 264 $a and $b
    * Enable commented field 264 test. Add new field 264 test using $a as well.
    * Fix no portion (#115)
    * change license to MIT
    * update punctuation tests to fixugen
    * init github actions
    * update package
    * 9.0.7-alpha.2
    * remove GNU license parts from files
    
    Co-authored-by: Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>

--- src/resolvable-ext-references-melinda.spec.js
+++ src/resolvable-ext-references-melinda.spec.js
@@ -1,32 +1,3 @@
-/**
- *
- * @licstart  The following is the entire license notice for the JavaScript code in this file.
- *
- * MARC record validators used in Melinda
- *
- * Copyright (c) 2014-2020 University Of Helsinki (The National Library Of Finland)
- *
- * This file is part of marc-record-validators-melinda
- *
- * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * marc-record-validators-melinda is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @licend  The above is the entire license notice
- * for the JavaScript code in this file.
- *
- */
-
-
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import {MarcRecord} from '@natlibfi/marc-record';
@@ -63,7 +34,8 @@ describe('resolvable-ext-references-melinda', () => {
 
   it('Throws an error when prefixPattern not provided', async () => {
     const validator = await testContext.default({endpoint, prefixPattern, fields});
-    await expect(validator.validate()).to.be.rejectedWith(Error, 'Cannot read property \'fields\' of undefined');
+    // Cannot read property 'fields' of undefined or Cannot read properties of undefined (reading 'fields')'
+    await expect(validator.validate()).to.be.rejectedWith(Error, /^Cannot read propert/u);
   });
 
   describe('#validate', () => {
--- test-fixtures/resolvable-ext-references-melinda.js
+++ test-fixtures/resolvable-ext-references-melinda.js
@@ -1,30 +1,3 @@
-/**
- *
- * @licstart  The following is the entire license notice for the JavaScript code in this file.
- *
- * MARC record validators used in Melinda
- *
- * Copyright (c) 2014-2020 University Of Helsinki (The National Library Of Finland)
- *
- * This file is part of marc-record-validators-melinda
- *
- * marc-record-validators-melinda program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * marc-record-validators-melinda is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @licend  The above is the entire license notice
- * for the JavaScript code in this file.
- *
- */
 
 export const fixture5000 = `
 <?xml version="1.0"?>
