2ddfba97936b412d8bd56db5f7038161ed022d4d
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2022-10-04 09:42:00 +0300

    fix duplicates-ind1

--- src/duplicates-ind1.spec.js
+++ src/duplicates-ind1.spec.js
@@ -1,94 +1,48 @@
 import {expect} from 'chai';
 import {MarcRecord} from '@natlibfi/marc-record';
-import validatorFactory from '../src/duplicates-ind1';
+import validatorFactory from './duplicates-ind1';
 
-describe('duplicates-ind1', () => {
-  it('Creates a validator', async () => {
-    const validator = await validatorFactory(/^245$/u);
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
+import createDebugLogger from 'debug';
 
-    expect(validator)
-      .to.be.an('object')
-      .that.has.any.keys('description', 'validate');
-
-    expect(validator.description).to.be.a('string');
-    expect(validator.validate).to.be.a('function');
-  });
-
-  describe('#validate', () => {
-    it('Finds the record valid', async () => {
-      const validator = await validatorFactory(/^500$/u);
-      const record = new MarcRecord({
-        fields: [
-          {
-            tag: '500',
-            ind1: ' ',
-            ind2: '0',
-            subfields: [{code: 'a', value: 'foo'}]
-          },
-          {
-            tag: '500',
-            ind1: ' ',
-            ind2: '0',
-            subfields: [{code: 'a', value: 'foo'}]
-          }
-        ]
-      });
-      const result = await validator.validate(record);
-
-      expect(result).to.eql({valid: true, messages: []});
-    });
-    it('Finds the record invalid', async () => {
-      const validator = await validatorFactory(/^500$/u);
-      const record = new MarcRecord({
-        fields: [
-          {
-            tag: '500',
-            ind1: ' ',
-            ind2: '0',
-            subfields: [{code: 'a', value: 'foo'}]
-          },
-          {
-            tag: '500',
-            ind1: '1',
-            ind2: '0',
-            subfields: [{code: 'a', value: 'foo'}]
-          }
-        ]
-      });
-      const result = await validator.validate(record);
-
-      expect(result).to.eql({valid: false, messages: ['Multiple 500 fields which only differ in the first indicator']});
-    });
-  });
-
-  describe('#fix', () => {
-    it('Removes duplicate values', async () => {
-      const validator = await validatorFactory(/^500$/u);
-      const record = new MarcRecord({
-        fields: [
-          {
-            tag: '500',
-            ind1: ' ',
-            ind2: '0',
-            subfields: [{code: 'a', value: 'foo'}]
-          },
-          {
-            tag: '500',
-            ind1: '1',
-            ind2: '0',
-            subfields: [{code: 'a', value: 'foo'}]
-          }
-        ]
-      });
-      await validator.fix(record);
-      expect(record.fields).to.eql([
-        {
-          tag: '500',
-          ind1: ' ',
-          ind2: '0',
-          subfields: [{code: 'a', value: 'foo'}]
-        }
-      ]);
-    });
-  });
+generateTests({
+  callback,
+  path: [__dirname, '..', 'test-fixtures', 'duplicates-ind1'],
+  useMetadataFile: true,
+  recurse: false,
+  fixura: {
+    reader: READERS.JSON
+  },
+  mocha: {
+    before: () => testValidatorFactory()
+  }
 });
+
+const debug = createDebugLogger('@natlibfi/marc-record-validators-melinda/duplicates-ind1:test');
+
+async function testValidatorFactory() {
+  const validator = await validatorFactory(/^500$/u);
+
+  expect(validator)
+    .to.be.an('object')
+    .that.has.any.keys('description', 'validate');
+
+  expect(validator.description).to.be.a('string');
+  expect(validator.validate).to.be.a('function');
+}
+
+async function callback({getFixture, tagPattern, fix = false}) {
+  const validator = await validatorFactory(new RegExp(tagPattern, 'u'));
+  const record = new MarcRecord(getFixture('record.json'));
+  const expectedResult = getFixture('expectedResult.json');
+
+  if (!fix) {
+    const result = await validator.validate(record);
+    return expect(result).to.eql(expectedResult);
+  }
+
+  const fixedRecord = await validator.fix(record);
+  debug(fixedRecord);
+  expect(record.fields).to.eql(expectedResult);
+}
