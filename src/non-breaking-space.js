c262dece5846321c3f94707a81192243274226ed
Aatu Nykänen <27770287+aatuny@users.noreply.github.com>
2023-01-26 16:41:41 +0200

    Add non-breaking-space validator (#141)
    * - Added initial version of non-breaking-space validator
    
    * - Fixed fix-option to replace non-breaking space with space instead of empty string

---
+++ src/non-breaking-space.js
@@ -0,0 +1,36 @@
+/**
+ * Provides interface to validate and fix record fields, which include subfields that contain no-breaking space character
+ * @returns {Object} Object containing interfaces required by marc-record-validators-melinda package
+ */
+export default function () {
+  return {
+    description: 'Handles subfields that contains non-breaking space character',
+    validate,
+    fix
+  };
+
+  function validate(record) {
+    const nonValidFields = record.fields.filter(({subfields}) => subfields.filter(valueContainsNonBreakingSpace).length > 0);
+
+    const valid = nonValidFields.length === 0;
+    const messages = nonValidFields.flatMap(({tag, subfields}) => subfields.map(sf => `Field ${tag} subfield $${sf.code} contains non-breaking space character(s)`));
+
+    return valid ? {valid, messages: []} : {valid, messages};
+  }
+
+  /* eslint-disable functional/immutable-data,functional/no-conditional-statement */
+  function fix(record) {
+    record.fields.forEach(({subfields}) => {
+      subfields.forEach(subfield => {
+        if (valueContainsNonBreakingSpace(subfield)) {
+          subfield.value = subfield.value.replaceAll(/\u00A0/gu, ' ');
+        }
+      });
+    });
+  }
+  /* eslint-enable functional/immutable-data,functional/no-conditional-statement */
+
+  function valueContainsNonBreakingSpace({value}) {
+    return (/\u00A0/u).test(value);
+  }
+}
