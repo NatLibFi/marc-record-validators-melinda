a66ee15e1eeeef9819045f10ac9f5a59e7bc6fa1
nvolk <nicholas.volk@helsinki.fi>
2023-03-04 00:14:54 +0200

    typo fixes plus move one function to subfield6Utils.js

--- src/removeOrphanedSubfield6s.js
+++ src/removeOrphanedSubfield6s.js
@@ -1,23 +1,23 @@
 import createDebugLogger from 'debug';
-import {fieldHasSubfield, fieldToString, isValidSubfield6, nvdebug, resetSubfield6OccurenceNumber, subfield6GetOccurenceNumber, subfieldToString} from './subfield6Utils';
+import {fieldHasSubfield, fieldHasWantedTagAndOccurrenceNumber, fieldToString, isValidSubfield6, nvdebug, resetSubfield6OccurrenceNumber, subfield6GetOccurrenceNumber, subfieldToString} from './subfield6Utils';
 
 // Relocated from melinda-marc-record-merge-reducers (and renamed)
 
-const debug = createDebugLogger('@natlibfi/marc-record-validators-melinda:reindexSubfield6OccurenceNumbers');
+const debug = createDebugLogger('@natlibfi/marc-record-validators-melinda:reindexSubfield6OccurrenceNumbers');
 
 export default function () {
   return {
-    description: 'Remove occurence-number-orphaned $6 subfields. In field 880, occurence number becomes 00',
+    description: 'Remove occurrence-number-orphaned $6 subfields. In field 880, occurrence number becomes 00',
     validate, fix
   };
 
   function fix(record) {
-    nvdebug('Fix SF6 orphaned occurence numbers');
+    nvdebug('Fix SF6 orphaned occurrence numbers');
     const res = {message: [], fix: [], valid: true};
     //message.fix = []; // eslint-disable-line functional/immutable-data
 
     // This can not really fail...
-    recordFixSubfield6OccurenceNumbers(record);
+    recordFixSubfield6OccurrenceNumbers(record);
 
     //message.valid = !(message.message.length >= 1); // eslint-disable-line functional/immutable-data
     return res;
@@ -25,7 +25,7 @@ export default function () {
 
   function validate(record) {
     // Check max, and check number of different indexes
-    nvdebug('Validate SF6 orphaned occurence numbers', debug);
+    nvdebug('Validate SF6 orphaned occurrence numbers', debug);
     const fieldsContainingSubfield6 = record.fields.filter(field => fieldHasSubfield(field, '6'));
 
     const orphanedFields = getOrphanedFields(fieldsContainingSubfield6);
@@ -33,21 +33,21 @@ export default function () {
     const res = {message: []};
 
     if (orphanedFields.length > 0) { // eslint-disable-line functional/no-conditional-statement
-      res.message = [`${orphanedFields.length} orphaned occurence number field(s) detected`]; // eslint-disable-line functional/immutable-data
+      res.message = [`${orphanedFields.length} orphaned occurrence number field(s) detected`]; // eslint-disable-line functional/immutable-data
     }
     res.valid = res.message.length < 1; // eslint-disable-line functional/immutable-data
     return res;
   }
 }
 
-export function recordFixSubfield6OccurenceNumbers(record) {
+export function recordFixSubfield6OccurrenceNumbers(record) {
   const fieldsContainingSubfield6 = record.fields.filter(field => fieldHasSubfield(field, '6'));
   const orphanedFields = getOrphanedFields(fieldsContainingSubfield6);
 
   orphanedFields.forEach(field => fieldFixOrphanedSubfields(field));
 
   function fieldFixOrphanedSubfields(field) {
-    // Field 880: orphaned $6 subfields: occurence number is changed to '00':
+    // Field 880: orphaned $6 subfields: occurrence number is changed to '00':
     if (field.tag === '880') {
       field.subfields.forEach(sf => field880FixOrphanedSubfield(sf));
       return;
@@ -65,24 +65,13 @@ export function recordFixSubfield6OccurenceNumbers(record) {
     if (!isOrphanedSubfield(subfield, '880', fieldsContainingSubfield6)) {
       return;
     }
-    // convert occurence number to 00
-    resetSubfield6OccurenceNumber(subfield, '00');
+    // convert occurrence number to 00
+    resetSubfield6OccurrenceNumber(subfield, '00');
   }
 }
 
 
-function hasWantedTagAndOccurenceNumber(subfield, tagAndOccurenceNumber) {
-  if (subfield.code !== '6') {
-    return false;
-  }
-  // We could also use generic code and go getTag()+'-'+getIndex() instead of regexp...
-  const key = subfield.value.replace(/^([0-9][0-9][0-9]-[0-9][0-9]+).*$/u, '$1'); // eslint-disable-line prefer-named-capture-group
-  nvdebug(` Compare '${key}' vs '${tagAndOccurenceNumber}'`);
-  return key === tagAndOccurenceNumber;
-}
-
-
-function findPairForSubfield6OccurenceNumber(subfield6, myTag, candPairFields) {
+function findPairForSubfield6OccurrenceNumber(subfield6, myTag, candPairFields) {
   // We keep the crap!
   if (!isValidSubfield6(subfield6)) {
     return undefined;
@@ -93,14 +82,14 @@ function findPairForSubfield6OccurenceNumber(subfield6, myTag, candPairFields) {
   // Only valid $6 value that fails to map to another field is iffy...
   const referredTag = subfield6.value.substring(0, 3);
 
-  const occurenceNumber = subfield6GetOccurenceNumber(subfield6);
-  if (occurenceNumber === '00') {
+  const occurrenceNumber = subfield6GetOccurrenceNumber(subfield6);
+  if (occurrenceNumber === '00') {
     return undefined;
   }
-  const tagAndOccurenceNumber = `${myTag}-${occurenceNumber}`;
-  nvdebug(`Try to find occurence number ${tagAndOccurenceNumber} in field ${referredTag}...`);
-  //const relevantFields = fields.filter(field => field.tag === referredTag && field.subfields.some(sf => subfield6GetOccurenceNumber(sf) === occurenceNumber));
-  const relevantFields = candPairFields.filter(field => field.tag === referredTag && field.subfields.some(sf => hasWantedTagAndOccurenceNumber(sf, tagAndOccurenceNumber)));
+  const tagAndOccurrenceNumber = `${myTag}-${occurrenceNumber}`;
+  nvdebug(`Try to find occurrence number ${tagAndOccurrenceNumber} in field ${referredTag}...`);
+  //const relevantFields = fields.filter(field => field.tag === referredTag && field.subfields.some(sf => subfield6GetOccurrenceNumber(sf) === occurrenceNumber));
+  const relevantFields = candPairFields.filter(field => field.tag === referredTag && fieldHasWantedTagAndOccurrenceNumber(field, tagAndOccurrenceNumber));
   if (relevantFields.length === 0) {
     return undefined;
   }
@@ -109,10 +98,10 @@ function findPairForSubfield6OccurenceNumber(subfield6, myTag, candPairFields) {
 }
 
 function isOrphanedSubfield(subfield, tag, pairCandidateFields) {
-  if (!isValidSubfield6(subfield) || subfield6GetOccurenceNumber(subfield) === '00') {
+  if (!isValidSubfield6(subfield) || subfield6GetOccurrenceNumber(subfield) === '00') {
     return false;
   }
-  return !findPairForSubfield6OccurenceNumber(subfield, tag, pairCandidateFields);
+  return !findPairForSubfield6OccurrenceNumber(subfield, tag, pairCandidateFields);
 }
 
 
