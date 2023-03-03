ecd62c4d9e081cef37e7c76b5756d47732a225a1
nvolk <nicholas.volk@helsinki.fi>
2023-03-03 14:36:09 +0200

    Support new $6 occurence number reindex valitor/fixer

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import IsbnIssn from './isbn-issn';
 import ItemLanguage from './item-language';
 import NormalizeUTF8Diacritics from './normalize-utf8-diacritics';
 import Punctuation from './punctuation/';
+import reindexSubfield6OccurenceNumbers from './reindexSubfield6OccurenceNumbers';
 import ResolvableExtReferences from './resolvable-ext-references-melinda';
 import SortTags from './sort-tags';
 import SubfieldExclusion from './subfield-exclusion';
@@ -33,6 +34,7 @@ export {
   ItemLanguage,
   NormalizeUTF8Diacritics,
   Punctuation,
+  reindexSubfield6OccurenceNumbers,
   ResolvableExtReferences,
   SortTags,
   SubfieldExclusion,
