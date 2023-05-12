222f2d1f3a0c9d7458238a7f808c287f90f8efcb
Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>
2023-05-12 15:56:33 +0300

    init typeOfDate-008 and tests (#179)
    * add validator typeOfDate-008 and tests

---
+++ src/typeOfDate-008.js
@@ -0,0 +1,40 @@
+import createDebugLogger from 'debug';
+
+const debug = createDebugLogger('@natlibfi/marc-record-validators-melinda/typeOfDate-008');  // eslint-disable-line
+
+export default function () {
+  return {
+    description: 'Validates 008 06',
+    validate,
+    fix
+  };
+
+  function validate(record) {
+    const [f008] = record.get(/008/u);
+    const c06 = f008.value.substring(6, 7);
+    const c1114 = f008.value.substring(11, 15);
+    // if 008 06 = s, and 11-14 = #### (not year/digits)
+    if (c06 === 't' && !(/[0-9u]{4}/u).test(c1114)) {
+      debug('is t and not valid 1114');
+      return {valid: false, message: 'Invalid 008 06'};
+    }
+
+    return {valid: true};
+  }
+
+  function fix(record) {
+    //  LDR/06=t ja 11-14=####, niin LDR/06 muutetaan s:ksi
+    const [f008] = record.pop(/008/u); // eslint-disable-line functional/immutable-data
+    const c06 = f008.value.substring(6, 7);
+    const c1114 = f008.value.substring(11, 15);
+    // if 008 06 = s, and 11-14 = #### (not year/digits)
+    if (c06 === 't' && !(/[0-9u]{4}/u).test(c1114)) {
+      f008.value = `${f008.value.substring(0, 6)}s${f008.value.substring(7)}`; // eslint-disable-line functional/immutable-data
+      record.insertField(f008);
+      return true;
+    }
+
+    record.insertField(f008);
+    return true;
+  }
+}
---
+++ src/typeOfDate-008.spec.js
@@ -0,0 +1,47 @@
+import {expect} from 'chai';
+import {MarcRecord} from '@natlibfi/marc-record';
+import validatorFactory from './typeOfDate-008';
+import {READERS} from '@natlibfi/fixura';
+import generateTests from '@natlibfi/fixugen';
+import createDebugLogger from 'debug';
+
+generateTests({
+  callback,
+  path: [__dirname, '..', 'test-fixtures', 'typeOfDate-008'],
+  useMetadataFile: true,
+  recurse: false,
+  fixura: {
+    reader: READERS.JSON
+  },
+  mocha: {
+    before: () => testValidatorFactory()
+  }
+});
+const debug = createDebugLogger('@natlibfi/marc-record-validators-melinda/typeOfDate-008:test');  // eslint-disable-line
+
+async function testValidatorFactory() {
+  const validator = await validatorFactory();
+
+  expect(validator)
+    .to.be.an('object')
+    .that.has.any.keys('description', 'validate');
+
+  expect(validator.description).to.be.a('string');
+  expect(validator.validate).to.be.a('function');
+}
+
+async function callback({getFixture, fix = false}) {
+  const validator = await validatorFactory();
+  const record = new MarcRecord(getFixture('record.json'));
+  const expectedResult = getFixture('expectedResult.json');
+  // console.log(expectedResult); // eslint-disable-line
+
+  if (!fix) {
+    const result = await validator.validate(record);
+    expect(result).to.eql(expectedResult);
+    return;
+  }
+
+  await validator.fix(record);
+  expect(record).to.eql(expectedResult);
+}
