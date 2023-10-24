c15e1aa54e4823aa89409947c7b4ab325ddc1855
Minttu Hurme <ammsalme@users.noreply.github.com>
2023-10-24 11:02:58 +0300

    Add NormalizeDashes & Update SortFields (#218) ; Fix f946 handling (#223) (#219)
    1) Add NormalizeDashes ; Update SortFields (#218)
    
    NormalizeDashes
    * Add validator NormalizeDashes [MELKEHITYS-2582] 		
    
    SortFields
    * Use general sorting functions from https://github.com/NatLibFi/marc-record-js/
    * Add Melinda specific local sorting:
        ** sortByIndexTerms
        ** preferFenniKeep
        ** sortByRelatorTerm
        ** sortByFieldLinkAndSequenceNumber
        ** sortByOccurenceNumber 
    
    * Update deps
    
    2) Fix f946 handling [MET-495] (#223)
    
    RemoveInferiorDatafields
    * Keep f946 with subfield $i 'Nimeke Onixissa' in automatically created records (encoding level '2' & 500 $a 'Koneellisesti tuotettu tietue') even if its contents are identical to f245/f246 title (MET-495)
    
    3) 
    
    * Update deps
    * 10.15.0-alpha.3
    
    ---------
    
    Co-authored-by: nvolk <nicholas.volk@helsinki.fi>

---
+++ src/normalize-dashes.js
@@ -0,0 +1,69 @@
+//import createDebugLogger from 'debug';
+import clone from 'clone';
+import {fieldToString, isControlSubfieldCode, nvdebug} from './utils';
+
+// Author(s): Nicholas Volk
+export default function () {
+
+  return {
+    description: 'Normalize various dashes to "-"',
+    validate, fix
+  };
+
+  function fix(record) {
+    nvdebug(`FIX ME`);
+    record.fields.forEach(field => {
+      fixDashes(field);
+    });
+
+    const res = {message: [], fix: [], valid: true};
+    return res;
+  }
+
+  function validate(record) {
+    const res = {message: []};
+
+    nvdebug(`VALIDATE ME`);
+    record.fields?.forEach(field => {
+      validateField(field, res);
+    });
+
+    res.valid = !(res.message.length >= 1); // eslint-disable-line functional/immutable-data
+    return res;
+  }
+
+  function validateField(field, res) {
+    const orig = fieldToString(field);
+    nvdebug(` VALIDATE FIELD '${orig}'`);
+
+    const normalizedField = fixDashes(clone(field));
+    const mod = fieldToString(normalizedField);
+    if (orig === mod) { // Fail as the input is "broken"/"crap"/sumthing
+      return;
+    }
+    res.message.push(`'TODO: ${orig}' => '${mod}'`); // eslint-disable-line functional/immutable-data
+    return;
+  }
+}
+
+
+function fixDashes(field) {
+  if (!field.subfields) {
+    return field;
+  }
+
+  nvdebug(`Dashing ${fieldToString(field)}`);
+
+  field.subfields.forEach(sf => subfieldFixDashes(sf));
+
+  return field;
+
+  function subfieldFixDashes(subfield) {
+    if (isControlSubfieldCode(subfield.code)) {
+      return;
+    }
+    // Normalize dashes U+2010 ... U+2015 to '-':
+    subfield.value = subfield.value.replace(/[\u2010\u2011\u2012\u2013\u2014\u2015]/ug, '-'); // eslint-disable-line functional/immutable-data
+  }
+
+}
