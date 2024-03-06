6b7ea0d93c16b55957c292597d068e7babb31797
nvolk <nicholas.volk@helsinki.fi>
2024-03-06 23:05:46 +0200

    Add initial 337 support

--- src/utils33X.js
+++ src/utils33X.js
@@ -2,9 +2,21 @@ import fs from 'fs';
 import path from 'path';
 
 const mappings336 = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'src', 'field336.json'), 'utf8'));
+const mappings337 = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'src', 'field337.json'), 'utf8'));
 
 export function map336CodeToTerm(code, catLang = 'fin') {
   const [mapping] = mappings336.filter(m => m.code === code);
+  if (!mapping) {
+    return undefined;
+  }
+  if (catLang in mapping) {
+    return mapping[catLang];
+  }
+  return mapping.fin; // Default to Finnish (which should be complete, knock-knock)
+}
+
+export function map337CodeToTerm(code, catLang = 'fin') {
+  const [mapping] = mappings337.filter(m => m.code === code);
   if (!mapping) {
     return undefined;
   }
