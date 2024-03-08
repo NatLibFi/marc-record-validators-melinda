8d56880f7ed254dcd3e5e8859d379d3d6b9c29fc
nvolk <nicholas.volk@helsinki.fi>
2024-03-08 14:45:36 +0200

    Improve isElectronicMaterial() code

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,7 @@
 export function isElectronicMaterial(record) {
   const f337s = record.get('337');
 
-  return f337s.some(field => {
-    const mediaTypeIsC = field.subfields.some(sub => sub.code === 'b' && sub.value === 'c');
-    const sourceIsRdamedia = field.subfields.some(sub => sub.code === '2' && sub.value === 'rdamedia');
-    return mediaTypeIsC && sourceIsRdamedia;
-  });
+  return f337s.length > 0 && f337s.some(f => fieldHasSubfield(f, 'b', 'c') && fieldHasSubfield(f, '2', 'rdamedia'));
 }
 
 export function nvdebug(message, func = undefined) {
