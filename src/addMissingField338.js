fea9de105bb517e30fdc26c0fd305c99e1050040
nvolk <nicholas.volk@helsinki.fi>
2024-03-13 13:38:30 +0200

    Tune. Add new rules.

--- src/addMissingField338.js
+++ src/addMissingField338.js
@@ -35,6 +35,53 @@ export default function () {
   }
 
 
+  function trimExtent(value) {
+    return value.replace(/\([^)]*\)/gu, '').replace(/[0-9]/gu, '').replace(/^ +/gu, '').replace(/ +$/gu, '').replace(/  +/gu, ' ');
+  }
+
+  function extractExtent(record) {
+    const [f300] = record.get('300');
+    if (!f300) {
+      return undefined;
+    }
+    const [a] = record.fields.filter(sf => sf.code === 'a');
+    if (!a) {
+      return undefined;
+    }
+    return trimExtent(a.value);
+  }
+
+  function extentToComputerCarrierType(record) {
+    const extent = extractExtent(record); // trimmed 300$a
+    if (extent && extent.match(/^(?:computer card|datorkort|minneskort|muistikortti)[^ ]*$/u)) { // Melinda only muistikortti
+      return 'ck';
+    }
+
+    return undefined;
+  }
+
+  function getComputerCarrierType(record) {
+    const formOfItem = getFormOfItem(record);
+
+    if (formOfItem === 'o') {
+      return 'cr';
+    }
+
+    const typeOfRecord = record.getTypeOfRecord();
+
+    if (typeOfRecord !== 'm' && !['o', 'q', 's'].includes(formOfItem)) {
+      return undefined;
+    }
+    // Check fields 300$a (extent), 256$a (computer file characteristics), 516$a (type of computer file or data note), and possible 245$h (medium)
+    const cand = extentToComputerCarrierType(record);
+    if (cand) {
+      return cand;
+    }
+
+    return undefined;
+  }
+
+
   function isUnmediatedVolume(record) {
     const typeOfRecord = record.getTypeOfRecord();
     if (!['a', 'c', 'e', 't'].includes(typeOfRecord)) {
@@ -48,7 +95,7 @@ export default function () {
     const fields336 = record.get('336');
 
     if (fields336 && fields336.some(f => f.subfields.some(sf => sf.code === 'b' && ['txt', 'cri'].includes(sf.value)))) {
-      return true;
+      return 'nc'; // HANDLE ARKKI! ETC...
     }
     // Add 300$a value-based guesses?
     return false;
@@ -65,20 +112,55 @@ export default function () {
     if (formOfItem === 'b') { // mikrokortti
       return 'he';
     }
+    if (formOfItem === 'c') { // Deviates from usemarcon-bookwhere-rda. Fix it? Check data?
+      return 'hg'; // Mikrokortti (läpinäkymätön). We have none in Melinda it seems... Add test?
+    }
+    // 'd' isoteksti
+    // 'e' sanomalehti (for CR only)
+    // 'f' pistekirjoitus
+    if (formOfItem === 'o') { // online resource
+      return 'cr'; // add test!
+    }
+    // if (formOfItem === 'q') { // local electronic stuff, use 300 et al to guess
+    // 'r' painojäljenne, arkki vs nide?
+    // if (formOfItem === 's') { // electronic, either 'o' or 'q'... Can share some code
+
+    //if (formOfItem === 'q') {
+    // 300 USB should map to muistikortti/ck. Small sample implies that there are lots of errors in our data.
+    //}
+    return undefined;
+  }
+
+  function typeOfRecordToField338(record) {
+    const typeOfRecord = record.getTypeOfRecord(record);
+    if (typeOfRecord === 'r') {
+      // The only subdivision might be card/no. Marginal, so I'm not checking that now.
+      return 'nr';
+    }
+
     return undefined;
   }
 
   function guessMissing338B(record) {
-    const guessBasedOnFormOfItem = formOfItemToField338(record);
-    if (guessBasedOnFormOfItem) {
-      return guessBasedOnFormOfItem;
-    }
+    // Data sources (which should be trusted most?)
+    // LDR/06
+    // 007
+    // 008/form of item
+    // 300
+    // 256
+    // 516
+    // 245$h...
+    // First use form of item?
+    return getComputerCarrierType(record) || typeOfRecordToField338(record) || formOfItemToField338(record) || isUnmediatedVolume(record);
 
-    if (isUnmediatedVolume(record)) {
-      return 'nc';
+    /*
+    const firstFunction = guessFunctions.find(f => f(record));
+    if (firstFunction) {
+      return firstFunction(record);
     }
 
     return undefined;
+    */
   }
 
   function getMissing338(record) {
