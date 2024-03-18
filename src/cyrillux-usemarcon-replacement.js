736ec4eacce843da3fd1a1c49b935a4a90f6fcbc
nvolk <nicholas.volk@helsinki.fi>
2024-03-18 14:51:21 +0200

    Add cyrillux specific LDR fixes and field removal. Sort subfields in each field 028.

--- src/cyrillux-usemarcon-replacement.js
+++ src/cyrillux-usemarcon-replacement.js
@@ -1,5 +1,5 @@
 /*
-* stripPunctuation.js -- try and remove a marc field punctuation (based on and reverse of punctuation2.js)
+*  cyrillux-usemarcon-replacement.js -- implement and improve https://github.com/NatLibFi/USEMARCON-Cyrillux/tree/master
 *
 * Author(s): Nicholas Volk <nicholas.volk@helsinki.fi>
 *
@@ -8,6 +8,8 @@
 import {default as add336} from './addMissingField336';
 import {default as add337} from './addMissingField337';
 import {default as add338} from './addMissingField338';
+import {default as fixCountryCodes} from './fix-country-codes';
+import {sortAdjacentSubfields} from './sortSubfields';
 
 
 // import createDebugLogger from 'debug';
@@ -16,6 +18,9 @@ import {nvdebug} from './utils';
 // const debug = createDebugLogger('@natlibfi/marc-record-validators-melinda/punctuation2');
 
 const description = 'Replacement for Cyrillux usemarcon rules';
+
+const skipTags = ['001', '003', '010', '012', '014', '015', '016', '019', '025', '029', '032', '035', '036', '037', '038', '042', '049', '051', '061', '068', '071', '074', '079', '090', '091', '092', '094', '095', '096', '097', '099', '249', '350', '400', '574', '575', '577', '578', '589', '590', '591', '592', '593', '594', '595', '596', '597', '598', '599', '653', '698', '741', '742', '744', '790', '841', '842', '843', '844', '845', '850', '852', '853', '854', '855', '858', '859', '863', '864', '865', '856', '857', '858', '876', '877', '878', '882', '886', '887', '888', '890', '899'];
+
 export default function () {
   return {
     description, fix, validate
@@ -23,9 +28,30 @@ export default function () {
 
   function fix(record) {
     nvdebug(`${description}: fix`);
+    realFix(record);
+    const res = {message: [], fix: [], valid: true};
+    return res;
+  }
+
+  function realFix(record) {
+    // Fix leader: standard fixes + update LDR/17 to '4'
+    fixLeader(record);
+    record.leader = `${record.leader.substring(0, 17)}4${record.leader.substring(18, 24)}`; // eslint-disable-line functional/immutable-data
+
+    fixCountryCodes().fix(record); // 008/15-17
+    // ADD 008/35-37 check
+
+    // Field 028: use $b$a, not $a$b:
+    const f028 = record.fields.filter(f => f.tag === '028');
+    f028.forEach(f => sortAdjacentSubfields(f));
+
     add336().fix(record);
     add337().fix(record);
     add338().fix(record);
+
+    // Remove unwanted fields:
+    record.fields = record.fields.filter(f => !skipTags.includes(f.tag)); // eslint-disable-line functional/immutable-data
+
     const res = {message: [], fix: [], valid: true};
     return res;
   }
@@ -38,4 +64,8 @@ export default function () {
     return res;
   }
 
+}
+
+export function fixLeader(record) {
+  record.leader = `${record.leader.substring(0, 9)}a22${record.leader.substring(12, 18)}i${record.leader.substring(19, 20)}4500`; // eslint-disable-line functional/immutable-data
 }
