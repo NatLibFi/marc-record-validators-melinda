b5f83519d0be2f45bc2f7fdc644919573651c59a
nvolk <nicholas.volk@helsinki.fi>
2026-02-19 22:19:04 +0200

    X00/X10/X11/X30: make $i a paired subfield (-> merge less fields)

--- src/merge-fields/mergeConstraints.js
+++ src/merge-fields/mergeConstraints.js
@@ -17,7 +17,7 @@ const keyX00 =    'abcjklnoqrstuwx'; // Shared: $abcdefg...
 const keyX10 = 'abcdfghklnoprstuwx';
 const keyX11 = 'acdefghklnpqstuwx';
 const keyX30 = 'adfghklmnoprstvwxyz';
-const pairedTitleSubfields = 'klmnoprst'
+const pairedTitleSubfields = 'iklmnoprst'
 
 const mergeConstraints = [
   {'tag': '010', 'required': 'a', 'key': 'a'},
